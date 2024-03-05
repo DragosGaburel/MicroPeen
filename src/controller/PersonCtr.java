e11a04ab28934316328c11abc5b923b84a4d9a34
DragosGaburel <149156858+DragosGaburel@users.noreply.github.com>
2024-03-05 10:43:39 +0100

    person

--- src/controller/PersonCtr.java
+++ src/controller/PersonCtr.java
@@ -5,4 +5,6 @@ import db.PersonDBIF;
 public class PersonCtr {
 	private PersonDBIF personDB;
 	
+	
+	
 }
--- src/model/Person.java
+++ src/model/Person.java
@@ -16,6 +16,51 @@ public class Person {
 		this.phone = phone;
 		this.birthDate = birthDate;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", birthDate="
+				+ birthDate + "]";
+	}
 	
 	
 }
